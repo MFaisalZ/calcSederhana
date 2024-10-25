b007162a2701e3e98e9b2f2119224184b95e9950
MFaisalZ <40825206+MFaisalZ@users.noreply.github.com>
2024-10-26 00:06:45 +0700

    Create calc.vue

---
+++ calc.vue
@@ -0,0 +1,25 @@
+<template>
+  <div style="text-align: center;">
+    <input type="number" v-model.number="bilangan1" placeholder="Masukkan bilangan 1">
+    <input type="number" v-model.number="bilangan2" placeholder="Masukkan bilangan 2">
+    <button @click="hitung">HITUNG</button>
+    <input type="number" :value="hasil" disabled>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      bilangan1: 0,
+      bilangan2: 0,
+      hasil: 0
+    }
+  },
+  methods: {
+    hitung() {
+      this.hasil = this.bilangan1 + this.bilangan2;
+    }
+  }
+}
+</script>
